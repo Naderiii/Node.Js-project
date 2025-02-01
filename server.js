e816b1e3ea6f77c42e1b5363fdb02e4c0b297ca0
hedieh.naderi <hedieh.naderi@partsoftware.com>
2025-02-01 10:47:03 +0330

    Add MongoDB and Mongoose dependencies; implement database connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHander');
-const app = express();
+const connectDB = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
+
+connectDB();
+const app = express();
 
 const port = process.env.PORT || 5000;
 
