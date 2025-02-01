8a064e3a9bf8a8725e049e93dc2202f9c0b18c6c
hedieh.naderi <hedieh.naderi@partsoftware.com>
2025-02-01 13:23:32 +0330

    Add JWT authentication to user login and registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/uerModel");
 const bcryptjs = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 
 //@desc Register user
 //@route POST /api/users/register
@@ -24,6 +25,11 @@ const registerUser = asyncHandler(async (req, res) => {
     password: hashPassword
   });
   console.log(`User created ${user}`);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email })
+  } else {
+    throw new Error("User data is not valid");
+  }
   res.json({ message: "Register the user" });
 });
 
@@ -31,6 +37,26 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const user = await User.findOne({ email });
+  //compare password with hash password
+  if (user && (await bcryptjs.compare(password, user.password))) {
+    const accessToken = jwt.sign({
+      user: {
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      }
+    },
+      process.env.ACCESS_TOKEN_SECRET,
+      {expiresIn: "1m"}
+    );
+    res.status(200).json({ accessToken })
+  }
   res.json({ message: "Login user" });
 });
 
