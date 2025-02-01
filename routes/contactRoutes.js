dce9a9b2138a8fc26644bab2783aabd9ec683a3f
hedieh.naderi <hedieh.naderi@partsoftware.com>
2025-02-01 09:06:29 +0330

    Add error handling and validation for contact creation

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllContacts, createContact, getContact, updateContact, deleteContact } = require('../controllers/contactController')
+const { getAllContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact } = require('../controllers/contactController')
 
-router.route('/').get(getAllContacts);
-router.route('/').post(createContact);
-router.route('/:id').get(getContact);
-router.route('/:id').put(updateContact);
-router.route('/:id').delete(deleteContact);
+router.route('/').get(getAllContacts).post(createContact);
+router.route('/:id').get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
